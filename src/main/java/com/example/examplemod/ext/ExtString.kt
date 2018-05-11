d67d9417b822b6cfbb2262a76e05d4d7a44320cc
Unknown <dootydooter@gmail.com>
2018-05-11 16:33:20 -0500

    Extension string

--- src/main/java/com/example/examplemod/ext/ExtString.kt
+++ src/main/java/com/example/examplemod/ext/ExtString.kt
@@ -1,2 +1,27 @@
 package com.example.examplemod.ext
 
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraftforge.oredict.OreDictionary
+
+val String.toItemStack: ItemStack?
+    get() {
+        val sect = split(":")
+        if (sect.size !in 2..3) {
+            return null
+        }
+        // Retrieve meta
+        val meta = when (sect.size) {
+            2 -> try {
+                Integer.parseInt(sect[2])
+            } catch (e: Exception) {
+                OreDictionary.WILDCARD_VALUE
+            }
+            else -> 0
+        }
+        val item = Item.getByNameOrId("${sect[0]}:${sect[1]}")!!
+        return ItemStack(item, 1, meta)
+    }
+
+val String.isInOreDict: Boolean
+    get() = startsWith("ore:")
