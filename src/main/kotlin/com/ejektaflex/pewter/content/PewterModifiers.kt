323e7d4534bfb1921ec4187215765072e0493ced
Unknown <dootydooter@gmail.com>
2018-08-23 03:40:52 -0500

    Change modifiers to lazy load

--- src/main/kotlin/com/ejektaflex/pewter/content/PewterModifiers.kt
+++ src/main/kotlin/com/ejektaflex/pewter/content/PewterModifiers.kt
@@ -1,22 +1,34 @@
 package com.ejektaflex.pewter.content
 
+import com.ejektaflex.pewter.Pewter
+import com.ejektaflex.pewter.modifiers.armor.ArmorModAmber
 import com.ejektaflex.pewter.modifiers.tools.*
 import slimeknights.tconstruct.library.modifiers.Modifier
 
 object PewterModifiers {
-    val content: List<Modifier>
-        get() {
-            return listOf(
-                    ModSanitizing(),
+    val content: List<Modifier> by lazy {
+        // Regular Modifiers
+        val mods = mutableListOf<Modifier>(
+                // Thaumcraft
+                ModSanitizing(),
+                // Biomes O' Plenty
+                ModEnderAmethyst(),
+                ModRuby(),
+                ModPeridot(),
+                ModTopaz(),
+                ModTanzanite(),
+                ModMalachite(),
+                ModSapphire(),
+                ModAmber()
+        )
 
-                    ModEnderAmethyst(),
-                    ModRuby(),
-                    ModPeridot(),
-                    ModTopaz(),
-                    ModTanzanite(),
-                    ModMalachite(),
-                    ModSapphire(),
-                    ModAmber()
+        // Construct's Armory Integration
+        if (Pewter.CONFIG.MAIN.conarmIntegration) {
+            mods += mutableListOf<Modifier>(
+                    ArmorModAmber()
             )
         }
+
+        return@lazy mods
+    }
 }
